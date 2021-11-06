96f20468ba03cd92b410fbe19a6fe7112f0de802
Yang Fan <yangfan@jiaoyu361.com>
2021-11-06 12:32:40 +0800

    升级到 abp 4.4.3

--- src/Fan.Abp.EventBus.MediatR/Fan/Abp/EventBus/Local/MediatRLocalEventBus.cs
+++ src/Fan.Abp.EventBus.MediatR/Fan/Abp/EventBus/Local/MediatRLocalEventBus.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.EventBus;
 using Volo.Abp.EventBus.Local;
 using Volo.Abp.MultiTenancy;
+using Volo.Abp.Uow;
 
 namespace Fan.Abp.EventBus.Local
 {
@@ -23,41 +27,61 @@ namespace Fan.Abp.EventBus.Local
         /// <param name="options"></param>
         /// <param name="serviceScopeFactory"></param>
         /// <param name="currentTenant"></param>
+        /// <param name="errorHandler"></param>
         public MediatRLocalEventBus(IOptions<AbpLocalEventBusOptions> options, IServiceScopeFactory serviceScopeFactory,
-            ICurrentTenant currentTenant) : base(options, serviceScopeFactory, currentTenant)
+            ICurrentTenant currentTenant, IEventErrorHandler errorHandler) : base(options, serviceScopeFactory,
+            currentTenant, errorHandler)
         {
 
         }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="eventType"></param>
-        /// <param name="eventData"></param>
-        /// <param name="exceptions"></param>
-        /// <returns></returns>
-        protected override async Task TriggerHandlersAsync(Type eventType, object eventData, List<Exception> exceptions)
+        public override async Task PublishAsync(LocalEventMessage localEventMessage)
         {
+            await TriggerHandlersAsync(localEventMessage.EventType, localEventMessage.EventData, errorContext =>
+            {
+                errorContext.EventData = localEventMessage.EventData;
+                errorContext.SetProperty(nameof(LocalEventMessage.MessageId), localEventMessage.MessageId);
+            });
+        }
+
+
+        public override async Task TriggerHandlersAsync(Type eventType, object eventData, Action<EventExecutionErrorContext> onErrorAction = null)
+        {
+            var exceptions = new List<Exception>();
+
             if (typeof(INotification).IsAssignableFrom(eventType))
             {
-                try
-                {
-                    using var scope = ServiceScopeFactory.CreateScope();
-                    await scope.ServiceProvider.GetService<IPublisher>().Publish(eventData);
-                }
-                catch (TargetInvocationException ex)
-                {
-                    exceptions.Add(ex.InnerException);
-                }
-                catch (Exception ex)
-                {
-                    exceptions.Add(ex);
-                }
-
-                return;
+                await TriggerMediatRHandlersAsync(eventType, eventData, exceptions);
+            } 
+            else
+            {
+                await TriggerHandlersAsync(eventType, eventData, exceptions);
             }
 
-            await base.TriggerHandlersAsync(eventType, eventData, exceptions);
+            if (exceptions.Any())
+            {
+                var context = new EventExecutionErrorContext(exceptions, eventType, this);
+                onErrorAction?.Invoke(context);
+                await ErrorHandler.HandleAsync(context);
+            }
+        }
+
+
+        protected virtual async Task TriggerMediatRHandlersAsync(Type eventType, object eventData,
+            List<Exception> exceptions)
+        {
+            try
+            {
+                using var scope = ServiceScopeFactory.CreateScope();
+                await scope.ServiceProvider.GetService<IPublisher>()?.Publish(eventData);
+            }
+            catch (TargetInvocationException ex)
+            {
+                exceptions.Add(ex.InnerException);
+            }
+            catch (Exception ex)
+            {
+                exceptions.Add(ex);
+            }
         }
     }
 }
